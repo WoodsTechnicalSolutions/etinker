77b34aba9105b122f2ced2efcb6781768eaf5134
Derald D. Woods <woods.technical@gmail.com>
2018-05-12 18:14:10 +0000

    boards/omap3-evm: Add TI OMAP35XX EVM (TMDSEVM3530) board support

---
+++ boards/omap3-evm/etinker.mk
@@ -0,0 +1,35 @@
+#
+# TI OMAP35XX EVM, ARM Cortex-A8, board configuration file for 'etinker'
+#
+# Copyright (C) 2018 Derald D. Woods
+#
+# This file is made available under the Creative Commons CC0 1.0
+# Universal Public Domain Dedication.
+#
+# The person who associated a work with this deed has dedicated
+# the work to the public domain by waiving all of his or her rights
+# to the work worldwide under copyright law, including all related
+# and neighboring rights, to the extent allowed by law. You can copy,
+# modify, distribute and perform the work, even for commercial purposes,
+# all without asking permission.
+#
+# The board configs are intended to be adapted for use with your project,
+# which may be proprietary.  So unlike the project itself, they are
+# licensed Public Domain.
+#
+
+export ET_BOARD_TYPE := omap2plus
+
+include $(ET_DIR)/boards/$(ET_BOARD_TYPE)/etinker.mk
+
+ET_BOARD_TOOLCHAIN_TREE ?= crosstool-ng
+ET_BOARD_KERNEL_TREE ?= linux
+ET_BOARD_BOOTLOADER_TREE ?= u-boot-$(ET_BOARD)
+
+ET_BOARD_KERNEL_DT ?= omap3-evm
+
+ET_BOARD_KERNEL_LOADADDR ?= 0x82000000
+ET_BOARD_KERNEL_DEFCONFIG ?= omap2plus_defconfig
+
+ET_BOARD_BOOTLOADER_SPL_BINARY ?= MLO
+ET_BOARD_BOOTLOADER_DEFCONFIG ?= omap3_evm_defconfig
