8ce0b958f680b6d6cb166cb1ae4e8c036976369f
Derald D. Woods <woods.technical@gmail.com>
2018-06-17 15:21:12 -0500

    cryptodev: Fix start/stop init argument handling

--- common/rootfs/etc/modprobe.d/cryptodev.sh
+++ common/rootfs/etc/modprobe.d/cryptodev.sh
@@ -1,7 +1,16 @@
 module="cryptodev"
 ko="/lib/modules/`uname -r`/extra/$module.ko"
 if [ -f $ko ]; then
-	modprobe -v $module
+	case "$1" in
+	start)
+		modprobe -v $module
+		;;
+	stop)
+		modprobe -r $module
+		;;
+	*)
+		;;
+	esac
 	exit 0
 else
 	printf "modules: missing %s!\n" "$module"
