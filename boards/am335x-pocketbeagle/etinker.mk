82bc3da101aa97a9ef461289e11c1c2d184a3952
Derald D. Woods <woods.technical@gmail.com>
2018-08-14 02:58:46 +0000

    linux: bump to version 4.18.0

--- boards/am335x-pocketbeagle/etinker.mk
+++ boards/am335x-pocketbeagle/etinker.mk
@@ -37,7 +37,7 @@ export ET_BOARD_TYPE := omap2plus
 include $(ET_DIR)/boards/$(ET_BOARD_TYPE)/etinker.mk
 
 ET_BOARD_TOOLCHAIN_TREE ?= crosstool-ng
-ET_BOARD_KERNEL_TREE ?= linux-$(ET_BOARD)
+ET_BOARD_KERNEL_TREE ?= linux
 ET_BOARD_BOOTLOADER_TREE ?= u-boot-$(ET_BOARD)
 ET_BOARD_ROOTFS_TREE ?= buildroot
 
