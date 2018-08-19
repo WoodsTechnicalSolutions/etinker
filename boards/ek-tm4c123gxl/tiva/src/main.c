057d0c9abaef1a7228eee6bb578a11da34aa3aa1
Derald D. Woods <woods.technical@gmail.com>
2018-08-19 18:48:37 -0500

    ek-tm4c12{3g,94}xl: Add serial console from TivaC utils

--- boards/ek-tm4c123gxl/tiva/src/main.c
+++ boards/ek-tm4c123gxl/tiva/src/main.c
@@ -1,5 +1,5 @@
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
@@ -9,30 +9,76 @@
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
-#include "driverlib/can.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
 
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
 
-int main()
+void led_setup(void)
 {
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-	for (;;) {
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
-		ROM_SysCtlDelay(10000000);
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		ROM_SysCtlDelay(10000000);
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_GREEN);
-		ROM_SysCtlDelay(10000000);
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		ROM_SysCtlDelay(10000000);
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
-		ROM_SysCtlDelay(10000000);
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		ROM_SysCtlDelay(10000000);
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+}
+
+void console_setup(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+	UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+	UARTStdioConfig(0, 115200, 16000000);
+}
+
+#ifdef DEBUG
+void
+__error__(char *filename, uint32_t line)
+{
+	UARTprintf("%s:%lu\n", filename, line);
+}
+#endif
+
+int main(void)
+{
+	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+	led_setup();
+
+	console_setup();
+
+	while (true) {
+		UARTprintf("\033[2K\033[2J\rLED_RGB: RED");
+
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
+
+		SysCtlDelay(20000000);
+
+		UARTprintf("\033[2K\033[2J\rLED_RGB: GREEN");
+
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_GREEN);
+
+		SysCtlDelay(20000000);
+
+		UARTprintf("\033[2K\033[2J\rLED_RGB: BLUE");
+
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
+
+		SysCtlDelay(20000000);
+
+		UARTprintf("\033[2K\033[2J\rLED_RGB: OFF");
+
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
+
+		SysCtlDelay(20000000);
 	}
+
+	return 0;
 }
--- boards/ek-tm4c1294xl/tiva/src/main.c
+++ boards/ek-tm4c1294xl/tiva/src/main.c
@@ -1,5 +1,5 @@
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
@@ -9,30 +9,69 @@
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
-#include "driverlib/can.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
 
-#define LED_RED GPIO_PIN_1
-#define LED_BLUE GPIO_PIN_2
-#define LED_GREEN GPIO_PIN_3
+#define LED_D1 GPIO_PIN_1
+#define LED_D2 GPIO_PIN_0
+#define LED_D3 GPIO_PIN_4
+#define LED_D4 GPIO_PIN_0
 
-int main()
+void led_setup(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_D3|LED_D4);
+	GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, LED_D1|LED_D2);
+}
+
+void console_setup(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0|GPIO_PIN_1);
+
+	UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+	UARTStdioConfig(0, 115200, 16000000);
+}
+
+#ifdef DEBUG
+void
+__error__(char *filename, uint32_t line)
+{
+	UARTprintf("%s:%lu\n", filename, line);
+}
+#endif
+
+int main(void)
 {
 	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
 
-	for (;;) {
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED);
-		SysCtlDelay(10000000);
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		SysCtlDelay(10000000);
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_GREEN);
-		SysCtlDelay(10000000);
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		SysCtlDelay(10000000);
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_BLUE);
-		SysCtlDelay(10000000);
-		GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		SysCtlDelay(10000000);
+	led_setup();
+
+	console_setup();
+
+	while (true) {
+		UARTprintf("\033[2K\033[2J\rON: LED_D1 + LED_D3");
+
+		GPIOPinWrite(GPIO_PORTN_BASE, LED_D1|LED_D2, LED_D1|~LED_D2);
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_D3|LED_D4, LED_D3|~LED_D4);
+
+		SysCtlDelay(20000000);
+
+		UARTprintf("\033[2K\033[2J\rON: LED_D2 + LED_D4");
+
+		GPIOPinWrite(GPIO_PORTN_BASE, LED_D1|LED_D2, ~LED_D1|LED_D2);
+		GPIOPinWrite(GPIO_PORTF_BASE, LED_D3|LED_D4, ~LED_D3|LED_D4);
+
+		SysCtlDelay(20000000);
 	}
+
+	return 0;
 }
