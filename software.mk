48e2405a022a68154c83f81285a3f2f978029143
Derald D. Woods <woods.technical@gmail.com>
2019-06-15 12:35:13 -0500

    software: Verify that 'git clone' succeeded

--- software.mk
+++ software.mk
@@ -9,8 +9,8 @@
 
 # check for existence of a source tree
 define software-check
+	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] USING $(ET_SOFTWARE_DIR)/$1 for $2 *****\n\n"
 	@if ! [ -d $(ET_SOFTWARE_DIR)/$1 ]; then \
-		printf "\n"; \
 		mkdir -p $(ET_SOFTWARE_DIR); \
 		(cd $(ET_SOFTWARE_DIR) && \
 			url="`grep $2-url $(ET_DIR)/boards/$(ET_BOARD_TYPE)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
@@ -18,15 +18,14 @@ define software-check
 			case $2 in \
 			toolchain | bootloader | kernel | rootfs) \
 				git clone $$url $1; \
-				(cd $1 && git checkout $$ref); \
+				[ -d $1 ] && (cd $1 && git checkout $$ref) || exit 2; \
 				;; \
 			*) \
 				git clone $$url $2; \
-				(cd $2 && git checkout $$ref); \
+				[ -d $2 ] && (cd $2 && git checkout $$ref) || exit 2; \
 				;; \
 			esac; \
 		); \
 		printf "\n"; \
 	fi
-	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] USING $(ET_SOFTWARE_DIR)/$1 for $2 *****\n\n"
 endef
