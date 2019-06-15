41cf921daf3ca01b479f29f2ed5aeaae0f834b28
Derald D. Woods <woods.technical@gmail.com>
2019-06-14 23:55:14 -0500

    software: Clone missing Git repositories with 'software-check'

--- software.mk
+++ software.mk
@@ -1,7 +1,7 @@
 #
 # This is a GNU Make include for 'etinker'.
 #
-# Copyright (C) 2018 Derald D. Woods
+# Copyright (C) 2018-2019 Derald D. Woods
 #
 # This file is part of the Embedded Tinkerer Sandbox, and is made
 # available under the terms of the GNU General Public License version 3.
@@ -11,10 +11,22 @@
 define software-check
 	@if ! [ -d $(ET_SOFTWARE_DIR)/$1 ]; then \
 		printf "\n"; \
-		printf "*****  MISSING $(ET_SOFTWARE_DIR)/$1 DIRECTORY  *****\n"; \
-		printf "===>  PLEASE ADD $(ET_SOFTWARE_DIR)/$1 SOFTWARE  <===\n"; \
+		mkdir -p $(ET_SOFTWARE_DIR); \
+		(cd $(ET_SOFTWARE_DIR) && \
+			url="`grep $2-url $(ET_DIR)/boards/$(ET_BOARD_TYPE)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
+			ref="`grep $2-ref $(ET_DIR)/boards/$(ET_BOARD_TYPE)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
+			case $2 in \
+			toolchain | bootloader | kernel | rootfs) \
+				git clone $$url $1; \
+				(cd $1 && git checkout $$ref); \
+				;; \
+			*) \
+				git clone $$url $2; \
+				(cd $2 && git checkout $$ref); \
+				;; \
+			esac; \
+		); \
 		printf "\n"; \
-		exit 2; \
 	fi
-	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] USING $(ET_SOFTWARE_DIR)/$1 *****\n\n"
+	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] USING $(ET_SOFTWARE_DIR)/$1 for $2 *****\n\n"
 endef
