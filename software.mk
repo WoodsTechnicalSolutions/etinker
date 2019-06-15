ba91475414c490c04d53916ae63a962c5dc214a9
Derald D. Woods <woods.technical@gmail.com>
2019-06-15 13:58:50 -0500

    software: Make 'software.conf' file a board requirement

--- software.mk
+++ software.mk
@@ -7,14 +7,18 @@
 # available under the terms of the GNU General Public License version 3.
 #
 
+ifneq ($(shell ls $(ET_BOARD_DIR)/software.conf 2> /dev/null),$(ET_BOARD_DIR)/software.conf)
+$(error [ 'etinker' requires '$(ET_BOARD_DIR)/software.conf' ***)
+endif
+
 # check for existence of a source tree
 define software-check
 	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] USING $(ET_SOFTWARE_DIR)/$1 for $2 *****\n\n"
 	@if ! [ -d $(ET_SOFTWARE_DIR)/$1 ]; then \
 		mkdir -p $(ET_SOFTWARE_DIR); \
 		(cd $(ET_SOFTWARE_DIR) && \
-			url="`grep $2-url $(ET_DIR)/boards/$(ET_BOARD_TYPE)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
-			ref="`grep $2-ref $(ET_DIR)/boards/$(ET_BOARD_TYPE)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
+			url="`grep $2-url $(ET_BOARD_DIR)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
+			ref="`grep $2-ref $(ET_BOARD_DIR)/software.conf | cut -d ':' -f 2-3 | tr -d \\\\n`" && \
 			case $2 in \
 			toolchain | bootloader | kernel | rootfs) \
 				git clone $$url $1; \
