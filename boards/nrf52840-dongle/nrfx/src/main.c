1df435b9d3dca70706f7aaab1b8013989676e0ee
Derald D. Woods <woods.technical@gmail.com>
2020-02-10 21:25:17 -0600

    nrf52840-dongle: Add TWIM instance 1 on P0.{04,26}
    This code is enabled by setting 'ET_CFLAGS=-DUSE_TWIM_1' when issuing
    the make build command. It simply tries to read a byte from addreses
    0-127 on the I2C bus. A simple I2C I/O expander is used the verify
    things are working as expected. (i.e. PCF8575)

--- boards/nrf52840-dongle/nrfx/src/main.c
+++ boards/nrf52840-dongle/nrfx/src/main.c
@@ -7,6 +7,9 @@
 
 #include "nrfx_gpiote.h"
 #include "nrfx_spim.h"
+#if defined(USE_TWIM_1)
+#include "nrfx_twim.h"
+#endif
 #include "nrfx_systick.h"
 #include "nrfx_uarte.h"
 
@@ -19,6 +22,8 @@ nrfx_gpiote_out_config_t led_config = NRFX_GPIOTE_CONFIG_OUT_SIMPLE(true);
 
 static void gpio_init(void)
 {
+	nrfx_err_t err;
+
 	/**
 	 * pulled from Nordic SDK 16.0.0 'components/boards/boards.c'
 	 *
@@ -55,10 +60,9 @@ static void gpio_init(void)
 
 gpio_config:
 
-	if (nrfx_gpiote_init(NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY)
-							!= NRFX_SUCCESS) {
+	err = nrfx_gpiote_init(NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY);
+	if (err != NRFX_SUCCESS)
 		while (true);
-	}
 
 	nrfx_gpiote_out_init(LED1_G, &led_config);
 	nrfx_gpiote_out_init(LED2_R, &led_config);
@@ -68,20 +72,24 @@ gpio_config:
 
 int main(void)
 {
+	nrfx_err_t err;
+#if defined(USE_SPIM_0) || defined(USE_TWIM_1)
 	uint8_t i;
-#if !defined(USE_TWIM_1)
-	uint8_t tx[] = { 13, 10 }; // '\r\n'
 #endif
 	nrfx_uarte_t uarte_0 = NRFX_UARTE_INSTANCE(0);
 	nrfx_uarte_config_t uarte_0_config = NRFX_UARTE_DEFAULT_CONFIG(
 						UARTE_0_TX_PIN, UARTE_0_RX_PIN);
 #if defined(USE_TWIM_1)
-	// TODO: TWIM instance 1 declarations
+	nrfx_twim_t twim_1 = NRFX_TWIM_INSTANCE(1);
+	nrfx_twim_config_t twim_1_config = NRFX_TWIM_DEFAULT_CONFIG(
+						TWIM_1_SCL_PIN, TWIM_1_SDA_PIN);
 #else
 	nrfx_uarte_t uarte_1 = NRFX_UARTE_INSTANCE(1);
 	nrfx_uarte_config_t uarte_1_config = NRFX_UARTE_DEFAULT_CONFIG(
 						UARTE_1_TX_PIN, UARTE_1_RX_PIN);
 #endif
+#if defined(USE_SPIM_0)
+	uint8_t tx[] = { 13, 10 }; // '\r\n'
 	nrfx_spim_t spim_0 = NRFX_SPIM_INSTANCE(0);
 	nrfx_spim_config_t spim_0_config = {
 		.sck_pin        = SPIM_0_SCLK_PIN,
@@ -101,6 +109,7 @@ int main(void)
 		48, 49, 50, 51, 52, 53, 54, 55,
 		56, 57, 97, 98, 99, 100, 101, 102
 	};
+#endif
 
 	gpio_init();
 
@@ -111,9 +120,11 @@ int main(void)
 	printf("Starting ...\r\n");
 
 #if defined(USE_TWIM_1)
-	// TODO: TWIM instance 1 initialization
+	err = nrfx_twim_init(&twim_1, &twim_1_config, NULL, NULL);
 #else
-	if (nrfx_uarte_init(&uarte_1, &uarte_1_config, NULL) != NRFX_SUCCESS) {
+	err = nrfx_uarte_init(&uarte_1, &uarte_1_config, NULL);
+#endif
+	if (err != NRFX_SUCCESS) {
 		nrfx_gpiote_out_clear(LED2_G);
 		nrfx_gpiote_out_clear(LED2_B);
 		nrfx_gpiote_out_clear(LED1_G);
@@ -122,10 +133,13 @@ int main(void)
 			nrfx_gpiote_out_toggle(LED2_R);
 		}
 	}
+#if defined(USE_TWIM_1)
+	nrfx_twim_enable(&twim_1);
 #endif
 
-	if (nrfx_spim_init(&spim_0, &spim_0_config, NULL, NULL)
-							!= NRFX_SUCCESS) {
+#if defined(USE_SPIM_0)
+	err = nrfx_spim_init(&spim_0, &spim_0_config, NULL, NULL);
+	if (err != NRFX_SUCCESS) {
 		nrfx_gpiote_out_clear(LED2_G);
 		nrfx_gpiote_out_clear(LED2_B);
 		nrfx_gpiote_out_clear(LED1_G);
@@ -134,6 +148,7 @@ int main(void)
 			nrfx_gpiote_out_toggle(LED2_R);
 		}
 	}
+#endif
 
 	nrfx_gpiote_out_toggle(LED1_G);
 
@@ -156,6 +171,20 @@ int main(void)
 		nrfx_gpiote_out_clear(LED2_G);
 		nrfx_gpiote_out_set(LED2_B);
 
+#if defined(USE_TWIM_1)
+		for (i = 0; i < 128; i++) {
+			uint8_t data = 0;
+			nrfx_twim_xfer_desc_t xfer_desc = NRFX_TWIM_XFER_DESC_RX(i, &data, 1);
+			err = nrfx_twim_xfer(&twim_1, &xfer_desc, 0);
+			if (err != NRFX_SUCCESS)
+				continue;
+			while (nrfx_twim_is_busy(&twim_1));
+			if (data)
+				fprintf(stderr, "I2C device @ 0x%02x [0x%02x]\r\n", i, data);
+		}
+#endif
+
+#if defined(USE_SPIM_0)
 		for (i = 0; i < sizeof(spim_0_tx); i++) {
 			spim_0_xfer.p_rx_buffer = NULL;
 			spim_0_xfer.rx_length = 0;
@@ -171,5 +200,6 @@ int main(void)
 #if !defined(USE_TWIM_1)
 		nrfx_uarte_tx(&uarte_1, tx, sizeof(tx));
 #endif
+#endif // USE_SPI_0
 	}
 }
