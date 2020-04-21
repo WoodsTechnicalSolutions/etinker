cbc758b4e2a55f314c045db237ef9fa844119588
Derald D. Woods <woods.technical@gmail.com>
2020-04-21 17:41:21 -0500

    kernel.mk: Add support for cached defconfig

--- kernel.mk
+++ kernel.mk
@@ -15,7 +15,6 @@ endif
 export ET_KERNEL_TREE := $(ET_BOARD_KERNEL_TREE)
 export ET_KERNEL_DT := $(ET_BOARD_KERNEL_DT)
 export ET_KERNEL_LOADADDR := $(ET_BOARD_KERNEL_LOADADDR)
-export ET_KERNEL_DEFCONFIG := $(ET_BOARD_KERNEL_DEFCONFIG)
 export ET_KERNEL_SOFTWARE_DIR := $(ET_SOFTWARE_DIR)/$(ET_KERNEL_TREE)
 export ET_KERNEL_HEADERS_DIR ?= $(ET_SYSROOT_DIR)/usr/include
 export ET_KERNEL_CACHED_VERSION := $(shell grep -Po 'kernel-ref:\K[^\n]*' $(ET_BOARD_DIR)/software.conf)
@@ -64,12 +63,14 @@ export ET_KERNEL_LOCALVERSION
 export ET_KERNEL_BUILD_DIR := $(ET_DIR)/kernel/build/$(ET_BOARD_TYPE)/$(ET_CROSS_TUPLE)
 export ET_KERNEL_BUILD_BOOT_DIR := $(ET_KERNEL_BUILD_DIR)/arch/$(ET_ARCH)/boot
 export ET_KERNEL_BUILD_CONFIG := $(ET_KERNEL_BUILD_DIR)/.config
+export ET_KERNEL_BUILD_DEFCONFIG := $(ET_KERNEL_BUILD_DIR)/defconfig
 export ET_KERNEL_BUILD_SYSMAP := $(ET_KERNEL_BUILD_DIR)/System.map
 export ET_KERNEL_BUILD_DTB := $(ET_KERNEL_BUILD_BOOT_DIR)/dts/$(ET_KERNEL_DT).dtb
 export ET_KERNEL_BUILD_UIMAGE := $(ET_KERNEL_BUILD_BOOT_DIR)/uImage
 export ET_KERNEL_BUILD_ZIMAGE := $(ET_KERNEL_BUILD_BOOT_DIR)/zImage
 export ET_KERNEL_DIR := $(ET_DIR)/kernel/$(ET_BOARD)/$(ET_CROSS_TUPLE)
 export ET_KERNEL_CONFIG := $(ET_CONFIG_DIR)/$(ET_KERNEL_TREE)/config
+export ET_KERNEL_DEFCONFIG := $(ET_CONFIG_DIR)/$(ET_KERNEL_TREE)/et_$(ET_BOARD_TYPE)_defconfig
 export ET_KERNEL_SYSMAP := $(ET_KERNEL_DIR)/boot/System.map
 export ET_KERNEL_DTB := $(ET_KERNEL_DIR)/boot/$(ET_KERNEL_DT).dtb
 export ET_KERNEL_UIMAGE := $(ET_KERNEL_DIR)/boot/uImage
@@ -78,6 +79,12 @@ export ET_KERNEL_MODULES := $(ET_KERNEL_DIR)/lib/modules/$(ET_KERNEL_VERSION)$(E
 export ET_KERNEL_TARGET_FINAL ?= $(ET_KERNEL_MODULES)
 
 export CT_LINUX_CUSTOM_LOCATION := ${ET_KERNEL_SOFTWARE_DIR}
+
+ifeq ($(ET_BOARD_KERNEL_DEFCONFIG_CACHED),)
+et_kernel_defconfig := $(ET_BOARD_KERNEL_DEFCONFIG)
+else
+et_kernel_defconfig := $(ET_BOARD_KERNEL_DEFCONFIG_CACHED)
+endif
 
 define kernel-version
 	@printf "ET_KERNEL_VERSION: \033[0;33m[$(ET_KERNEL_CACHED_VERSION)]\033[0m $(ET_KERNEL_VERSION)\n"
@@ -118,10 +125,13 @@ define kernel-targets
 		if [ -f $(ET_KERNEL_CONFIG) ]; then \
 			rsync $(ET_KERNEL_CONFIG) $(ET_KERNEL_BUILD_CONFIG); \
 		else \
+			if [ -f $(ET_KERNEL_DEFCONFIG) ] && [ -n "$(ET_BOARD_KERNEL_DEFCONFIG_CACHED)" ]; then \
+				rsync $(ET_KERNEL_DEFCONFIG) $(ET_KERNEL_SOFTWARE_DIR)/arch/$(ET_ARCH)/configs/$(et_kernel_defconfig); \
+			fi; \
 			$(MAKE) --no-print-directory $(ET_CROSS_PARAMS) \
 				O=$(ET_KERNEL_BUILD_DIR) \
 				-C $(ET_KERNEL_SOFTWARE_DIR) \
-				$(ET_BOARD_KERNEL_DEFCONFIG); \
+				$(et_kernel_defconfig); \
 		fi; \
 	fi
 	$(call kernel-build,zImage)
@@ -134,7 +144,7 @@ endef
 define kernel-build
 	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] call kernel-build 'make $1' *****\n\n"
 	$(call kernel-depends)
-	@if [ -f $(ET_KERNEL_CONFIG) ] && ! [ "$1" = "$(ET_KERNEL_DEFCONFIG)" ]; then \
+	@if [ -f $(ET_KERNEL_CONFIG) ] && ! [ "$1" = "$(et_kernel_defconfig)" ]; then \
 		case "$1" in \
 		*config) \
 			rsync $(ET_KERNEL_CONFIG) $(ET_KERNEL_BUILD_CONFIG); \
@@ -202,6 +212,9 @@ define kernel-build
 	*config) \
 		if [ -f $(ET_KERNEL_BUILD_CONFIG) ]; then \
 			rsync $(ET_KERNEL_BUILD_CONFIG) $(ET_KERNEL_CONFIG); \
+			if [ -f $(ET_KERNEL_BUILD_DEFCONFIG) ]; then \
+				rsync $(ET_KERNEL_BUILD_DEFCONFIG) $(ET_KERNEL_DEFCONFIG); \
+			fi; \
 		else \
 			printf "***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] $(ET_KERNEL_TREE) .config MISSING! *****\n"; \
 			exit 2; \
@@ -254,7 +267,10 @@ define kernel-info
 	@printf "ET_KERNEL_UIMAGE: $(ET_KERNEL_UIMAGE)\n"
 	@printf "ET_KERNEL_ZIMAGE: $(ET_KERNEL_ZIMAGE)\n"
 	@printf "ET_KERNEL_CONFIG: $(ET_KERNEL_CONFIG)\n"
+	@printf "et_kernel_defconfig: $(et_kernel_defconfig)\n"
+	@printf "ET_KERNEL_DEFCONFIG: $(ET_KERNEL_DEFCONFIG)\n"
 	@printf "ET_KERNEL_BUILD_CONFIG: $(ET_KERNEL_BUILD_CONFIG)\n"
+	@printf "ET_KERNEL_BUILD_DEFCONFIG: $(ET_KERNEL_BUILD_DEFCONFIG)\n"
 	@printf "ET_KERNEL_BUILD_DTB: $(ET_KERNEL_BUILD_DTB)\n"
 	@printf "ET_KERNEL_BUILD_SYSMAP: $(ET_KERNEL_BUILD_SYSMAP)\n"
 	@printf "ET_KERNEL_BUILD_UIMAGE: $(ET_KERNEL_BUILD_UIMAGE)\n"
@@ -285,7 +301,7 @@ endif
 
 $(ET_KERNEL_CONFIG): $(ET_TOOLCHAIN_TARGET_FINAL)
 ifeq ($(shell test -f $(ET_KERNEL_CONFIG) && printf "EXISTS" || printf "DEFAULT"),DEFAULT)
-	$(call kernel-build,$(ET_KERNEL_DEFCONFIG))
+	$(call kernel-build,$(et_kernel_defconfig))
 endif
 
 .PHONY: kernel-clean
