9d0406049b68e3b9de95a35fee7e72ef4dcc48ec
Derald D. Woods <woods.technical@gmail.com>
2020-04-22 22:30:22 -0500

    bootloader.mk: Add support for cached defconfig

--- bootloader.mk
+++ bootloader.mk
@@ -14,8 +14,12 @@ endif
 # embedded bootloader, for application processors, is Das U-Boot
 export ET_BOOTLOADER_TREE := $(ET_BOARD_BOOTLOADER_TREE)
 export ET_BOOTLOADER_SOFTWARE_DIR := $(ET_SOFTWARE_DIR)/$(ET_BOOTLOADER_TREE)
-export ET_BOOTLOADER_DEFCONFIG := $(ET_BOARD_BOOTLOADER_DEFCONFIG)
 export ET_BOOTLOADER_CACHED_VERSION := $(shell grep -Po 'bootloader-ref:\K[^\n]*' $(ET_BOARD_DIR)/software.conf)
+ifeq ($(ET_BOARD_BOOTLOADER_DEFCONFIG_CACHED),)
+et_bootloader_defconfig := $(ET_BOARD_BOOTLOADER_DEFCONFIG)
+else
+et_bootloader_defconfig := $(ET_BOARD_BOOTLOADER_DEFCONFIG_CACHED)
+endif
 # [start] bootloader version magic
 ET_BOOTLOADER_VERSION := $(shell cd $(ET_BOOTLOADER_SOFTWARE_DIR) 2>/dev/null && git describe --dirty 2>/dev/null | tr -d v)
 ET_BOOTLOADER_LOCALVERSION := -$(shell cd $(ET_BOOTLOADER_SOFTWARE_DIR) 2>/dev/null && git describe --dirty 2>/dev/null | cut -d '-' -f 2-5)
@@ -38,11 +42,17 @@ ifeq ($(ET_BOOTLOADER_LOCALVERSION),-v$(ET_BOOTLOADER_VERSION))
 # exact tag in series (i.e. v2018.09)
 ET_BOOTLOADER_LOCALVERSION :=
 endif
+ifeq ($(ET_BOARD),pynq-z2-xlnx)
+# Xilinx zynq kernel, just use the tree as done with next
+ET_BOOTLOADER_VERSION := $(shell cd $(ET_BOOTLOADER_SOFTWARE_DIR) 2>/dev/null && make -s ubootversion | tr -d \\n)
+ET_BOOTLOADER_LOCALVERSION := -$(ET_BOOTLOADER_CACHED_VERSION)
+endif
 export ET_BOOTLOADER_VERSION
 export ET_BOOTLOADER_LOCALVERSION
 # [end] bootloader version magic
 export ET_BOOTLOADER_BUILD_DIR := $(ET_DIR)/bootloader/build/$(ET_BOARD)/$(ET_CROSS_TUPLE)
 export ET_BOOTLOADER_BUILD_CONFIG := $(ET_BOOTLOADER_BUILD_DIR)/.config
+export ET_BOOTLOADER_BUILD_DEFCONFIG := $(ET_BOOTLOADER_BUILD_DIR)/defconfig
 export ET_BOOTLOADER_BUILD_SYSMAP := $(ET_BOOTLOADER_BUILD_DIR)/System.map
 ifeq ($(shell echo $(ET_BOARD_TYPE)|grep omap2plus|cut -d '-' -f 1),omap2plus)
 export ET_BOOTLOADER_BUILD_SPL := $(ET_BOOTLOADER_BUILD_DIR)/$(ET_BOARD_BOOTLOADER_SPL_BINARY)
@@ -53,6 +63,7 @@ export ET_BOOTLOADER_BUILD_DTB := $(ET_BOOTLOADER_BUILD_DIR)/u-boot-dtb.img
 export ET_BOOTLOADER_BUILD_IMAGE := $(ET_BOOTLOADER_BUILD_DIR)/u-boot.img
 export ET_BOOTLOADER_DIR := $(ET_DIR)/bootloader/$(ET_BOARD)/$(ET_CROSS_TUPLE)
 export ET_BOOTLOADER_CONFIG := $(ET_CONFIG_DIR)/$(ET_BOOTLOADER_TREE)/config
+export ET_BOOTLOADER_DEFCONFIG := $(ET_CONFIG_DIR)/$(ET_BOOTLOADER_TREE)/$(et_bootloader_defconfig)
 export ET_BOOTLOADER_SYSMAP := $(ET_BOOTLOADER_DIR)/System.map
 export ET_BOOTLOADER_SPL := $(ET_BOOTLOADER_DIR)/boot/$(ET_BOARD_BOOTLOADER_SPL_BINARY)
 export ET_BOOTLOADER_DTB := $(ET_BOOTLOADER_DIR)/boot/u-boot-dtb.img
@@ -95,6 +106,9 @@ define bootloader-depends
 		rsync -rP $(ET_BOARD_DIR)/dts/u-boot/*.dts* \
 			$(ET_BOOTLOADER_SOFTWARE_DIR)/arch/$(ET_ARCH)/dts/; \
 	fi
+	@if [ -f $(ET_BOOTLOADER_DEFCONFIG) ]; then \
+		rsync $(ET_BOOTLOADER_DEFCONFIG) $(ET_BOOTLOADER_SOFTWARE_DIR)/configs/ > /dev/null; \
+	fi
 endef
 
 define bootloader-targets
@@ -104,11 +118,10 @@ define bootloader-targets
 		if [ -f $(ET_BOOTLOADER_CONFIG) ]; then \
 			rsync $(ET_BOOTLOADER_CONFIG) $(ET_BOOTLOADER_BUILD_CONFIG); \
 		else \
-			$(MAKE) --no-print-directory \
-				$(ET_CROSS_PARAMS) \
+			$(MAKE) --no-print-directory $(ET_CROSS_PARAMS) \
 				O=$(ET_BOOTLOADER_BUILD_DIR) \
 				-C $(ET_BOOTLOADER_SOFTWARE_DIR) \
-				$(ET_BOARD_BOOTLOADER_DEFCONFIG); \
+				$(et_board_bootloader_defconfig); \
 		fi; \
 	fi
 	$(call bootloader-build)
@@ -117,7 +130,7 @@ endef
 define bootloader-build
 	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] call bootloader-build 'make $1' *****\n\n"
 	$(call bootloader-depends)
-	@if [ -f $(ET_BOOTLOADER_CONFIG) ] && ! [ "$1" = "$(ET_BOOTLOADER_DEFCONFIG)" ]; then \
+	@if [ -f $(ET_BOOTLOADER_CONFIG) ] && ! [ "$1" = "$(et_bootloader_defconfig)" ]; then \
 		case "$1" in \
 		*config) \
 			rsync $(ET_BOOTLOADER_CONFIG) $(ET_BOOTLOADER_BUILD_CONFIG); \
@@ -127,8 +140,7 @@ define bootloader-build
 		esac; \
 	fi
 	(cd $(ET_BOOTLOADER_SOFTWARE_DIR) && \
-		$(MAKE) --no-print-directory -j $(ET_CPUS) \
-			$(ET_CROSS_PARAMS) \
+		$(MAKE) --no-print-directory -j $(ET_CPUS) $(ET_CROSS_PARAMS) \
 			DEVICE_TREE=$(DEVICE_TREE) \
 			LOCALVERSION=$(ET_BOOTLOADER_LOCALVERSION) \
 			O=$(ET_BOOTLOADER_BUILD_DIR) \
@@ -186,6 +198,9 @@ define bootloader-build
 		if [ -n "$(shell diff -q $(ET_BOOTLOADER_BUILD_CONFIG) $(ET_BOOTLOADER_CONFIG) 2> /dev/null)" ]; then \
 			rsync $(ET_BOOTLOADER_BUILD_CONFIG) $(ET_BOOTLOADER_CONFIG); \
 		fi; \
+		if [ -f $(ET_BOOTLOADER_BUILD_DEFCONFIG) ]; then \
+			rsync $(ET_BOOTLOADER_BUILD_DEFCONFIG) $(ET_BOOTLOADER_DEFCONFIG); \
+		fi; \
 	fi
 	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] bootloader-build 'make $1' done. *****\n\n"
 endef
@@ -225,7 +240,10 @@ define bootloader-info
 	@printf "ET_BOOTLOADER_DTB: $(ET_BOOTLOADER_DTB)\n"
 	@printf "ET_BOOTLOADER_IMAGE: $(ET_BOOTLOADER_IMAGE)\n"
 	@printf "ET_BOOTLOADER_CONFIG: $(ET_BOOTLOADER_CONFIG)\n"
+	@printf "et_bootloader_defconfig: $(et_bootloader_defconfig)\n"
+	@printf "ET_BOOTLOADER_DEFCONFIG: $(ET_BOOTLOADER_DEFCONFIG)\n"
 	@printf "ET_BOOTLOADER_BUILD_CONFIG: $(ET_BOOTLOADER_BUILD_CONFIG)\n"
+	@printf "ET_BOOTLOADER_BUILD_DEFCONFIG: $(ET_BOOTLOADER_BUILD_DEFCONFIG)\n"
 	@printf "ET_BOOTLOADER_BUILD_SYSMAP: $(ET_BOOTLOADER_BUILD_SYSMAP)\n"
 	@printf "ET_BOOTLOADER_BUILD_SPL: $(ET_BOOTLOADER_BUILD_SPL)\n"
 	@printf "ET_BOOTLOADER_BUILD_DTB: $(ET_BOOTLOADER_BUILD_DTB)\n"
@@ -256,7 +274,7 @@ endif
 
 $(ET_BOOTLOADER_CONFIG): $(ET_TOOLCHAIN_TARGET_FINAL)
 ifeq ($(shell test -f $(ET_BOOTLOADER_CONFIG) && printf "EXISTS" || printf "DEFAULT"),DEFAULT)
-	$(call bootloader-build,$(ET_BOOTLOADER_DEFCONFIG))
+	$(call bootloader-build,$(et_bootloader_defconfig))
 endif
 
 .PHONY: bootloader-clean
