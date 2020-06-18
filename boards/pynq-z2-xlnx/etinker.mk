c7c3059bafebd0ce43c33bfe9aa6ce384315a7ba
Derald D. Woods <woods.technical@gmail.com>
2020-06-17 23:09:25 -0500

    boards/pynq-z2-xlnx: bump to version xilinx-v2020.1

--- boards/pynq-z2-xlnx/etinker.mk
+++ boards/pynq-z2-xlnx/etinker.mk
@@ -43,6 +43,6 @@ ET_BOARD_KERNEL_LOADADDR ?= 0x02080000
 ET_BOARD_KERNEL_DEFCONFIG ?= xilinx_zynq_defconfig
 
 ET_BOARD_BOOTLOADER_SPL_BINARY ?= boot.bin
-ET_BOARD_BOOTLOADER_DEFCONFIG ?= zynq_zc702_defconfig
+ET_BOARD_BOOTLOADER_DEFCONFIG ?= xilinx_zynq_virt_defconfig
 
 ET_BOARD_DEFCONFIG := et_$(subst -,_,$(ET_BOARD))_defconfig
