37a8a67f589849dee914e3c0aeee5d32c6bc4604
Derald D. Woods <woods.technical@gmail.com>
2020-10-06 00:31:05 -0500

    u-boot: boards/meson*: Generate encrypted images with 'gxlimg' tool

--- boards/meson-libre/bootloader.mk
+++ boards/meson-libre/bootloader.mk
@@ -8,12 +8,22 @@ export ET_BOOTLOADER_BUILD_SPL := $(ET_BOOTLOADER_BUILD_DIR)/spl/$(ET_BOARD_BOOT
 export ET_BOOTLOADER_SPL := $(ET_BOOTLOADER_DIR)/boot/$(ET_BOARD_BOOTLOADER_SPL_BINARY)
 
 define bootloader-depends-$(ET_BOARD_TYPE)
+	$(call software-check,gxlimg,gxlimg)
+	$(call software-check,arm-trusted-firmware,arm-trusted-firmware)
 endef
 
 define bootloader-prepare-$(ET_BOARD_TYPE)
 endef
 
 define bootloader-finalize-$(ET_BOARD_TYPE)
+	@$(RM) $(ET_BOOTLOADER_DIR)/boot/u-boot*
+	@(cd $(ET_SOFTWARE_DIR)/gxlimg && \
+		$(MAKE) --no-print-directory \
+			clean \
+			image \
+			UBOOT=$(ET_BOOTLOADER_BUILD_IMAGE) && \
+		cp -av build/fip/gxl/u-boot.bin.enc $(ET_BOOTLOADER_DIR)/boot/u-boot.bin && \
+		cp -av build/gxl-boot.bin $(ET_BOOTLOADER_DIR)/boot/)
 endef
 
 define bootloader-info-$(ET_BOARD_TYPE)
