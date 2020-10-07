2d5ae93cd3dbaa663d55dde4e4f201eb959b372e
Derald D. Woods <woods.technical@gmail.com>
2020-10-06 21:44:56 -0500

    u-boot: boards/meson*: Move 'software-check' to 'finalize'

--- boards/meson-libre/bootloader.mk
+++ boards/meson-libre/bootloader.mk
@@ -8,14 +8,14 @@ export ET_BOOTLOADER_BUILD_SPL := $(ET_BOOTLOADER_BUILD_DIR)/spl/$(ET_BOARD_BOOT
 export ET_BOOTLOADER_SPL := $(ET_BOOTLOADER_DIR)/boot/$(ET_BOARD_BOOTLOADER_SPL_BINARY)
 
 define bootloader-depends-$(ET_BOARD_TYPE)
-	$(call software-check,gxlimg,gxlimg)
-	$(call software-check,arm-trusted-firmware,arm-trusted-firmware)
 endef
 
 define bootloader-prepare-$(ET_BOARD_TYPE)
 endef
 
 define bootloader-finalize-$(ET_BOARD_TYPE)
+	$(call software-check,gxlimg,gxlimg)
+	$(call software-check,arm-trusted-firmware,arm-trusted-firmware)
 	@$(RM) $(ET_BOOTLOADER_DIR)/boot/u-boot*
 	@(cd $(ET_SOFTWARE_DIR)/gxlimg && \
 		$(MAKE) --no-print-directory \
