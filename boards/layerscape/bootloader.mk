ad4f25b2ea511721086fba43b23de6605444e16a
Derald D. Woods <woods.technical@gmail.com>
2020-10-28 16:45:44 -0500

    boards/ls1043ardb: Add NXP Layerscape LS1043ARDB board support
    This provides a configuration that boots with Linux 5.9.1. U-Boot
    builds, but has not been verified yet. The proper sequence of operations
    to boot the board, with only a SD/MMC card, is TBD. Factory NOR flash is
    being used at the moment. (LSDK U-Boot 2017.07-g503eff0)
    
    The following U-Boot environment was added to load the upstream
    kernel and rootfs:
    
    -----------------------------------------------------------------
    bootcmd=run boot_uenv
    boot_uenv=mmc dev 0; if mmc rescan; then if fatload mmc 0 ${scriptaddr} uEnv.txt; then env import -t ${scriptaddr} ${filesize} && run uenvcmd; fi; fi;
    bootargs=console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootfstype=ext4 rootwait earlycon=uart8250,mmio,0x21c0500 mtdparts=60000000.nor:2m@0x100000(nor_bank0_uboot),40m@0x1100000(nor_bank0_fit),7m(nor_bank0_user),2m@0x4100000(nor_bank4_uboot),40m@0x5100000(nor_bank4_fit),-(nor_bank4_user);7e800000.flash:1m(nand_uboot),1m(nand_uboot_env),20m(nand_fit);spi0.0:1m(uboot),5m(kernel),1m(dtb),9m(file_system)
    -----------------------------------------------------------------
    
    This simply imports uEnv.txt from MMC and runs 'uenvcmd' like some OMAP3
    boards. The kernel and device-tree are then booted via 'booti'.

---
+++ boards/layerscape/bootloader.mk
@@ -0,0 +1,11 @@
+define bootloader-depends-$(ET_BOARD_TYPE)
+endef
+
+define bootloader-prepare-$(ET_BOARD_TYPE)
+endef
+
+define bootloader-finalize-$(ET_BOARD_TYPE)
+endef
+
+define bootloader-info-$(ET_BOARD_TYPE)
+endef
