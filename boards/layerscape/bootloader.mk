96073364407d9c42b1e72c2689c190726626b59d
Derald D. Woods <woods.technical@gmail.com>
2021-02-02 22:36:49 -0600

    boards: ls1043ardb: Use TFA sdboot as default

--- boards/layerscape/bootloader.mk
+++ boards/layerscape/bootloader.mk
@@ -3,6 +3,15 @@ export ET_BOOTLOADER_ARCH := arm
 # Handle out-of-tree devicetree build (i.e. dtb-y += fsl-ls1043a-custom-board.dtb)
 export DEVICE_TREE_MAKEFILE := -f $(ET_BOARD_DIR)/dts/u-boot/Makefile
 
+export LSDK_VERSION ?= LSDK-20.12
+export LSDK_VERSION_URL ?= lsdk2012
+export LSDK_MACHINE ?= ls1043ardb
+export LSDK_BOOTTYPE ?= sd
+export LSDK_FIRMWARE_BIN ?= $(ET_SOFTWARE_DIR)/qoriq/firmware_$(LSDK_MACHINE)_uboot_$(LSDK_BOOTTYPE)boot-$(LSDK_VERSION_URL).img
+export LSDK_SRK_HASH ?= $(ET_SOFTWARE_DIR)/qoriq/srk_hash-$(LSDK_VERSION_URL).txt
+export LSDK_SRK_PRI ?= $(ET_SOFTWARE_DIR)/qoriq/srk-$(LSDK_VERSION_URL).pri
+export LSDK_SRK_PUB ?= $(ET_SOFTWARE_DIR)/qoriq/srk-$(LSDK_VERSION_URL).pub
+
 define bootloader-depends-$(ET_BOARD_TYPE)
 endef
 
@@ -10,9 +19,49 @@ define bootloader-prepare-$(ET_BOARD_TYPE)
 endef
 
 define bootloader-finalize-$(ET_BOARD_TYPE)
-	$(call bootloader-build,u-boot.pbl)
-	@cp -av $(ET_BOOTLOADER_BUILD_DIR)/u-boot.pbl $(ET_BOOTLOADER_DIR)/boot/
+	@echo
+	@mkdir -p $(ET_SOFTWARE_DIR)/qoriq
+	@if ! [ -f $(LSDK_FIRMWARE_BIN) ]; then \
+		printf "***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) QorIQ Composite Firmware Image *****\n\n"; \
+		wget -O $(LSDK_FIRMWARE_BIN) \
+			https://www.nxp.com/lgfiles/sdk/$(LSDK_VERSION_URL)/firmware_$(LSDK_MACHINE)_uboot_$(LSDK_BOOTTYPE)boot.img; \
+		if ! [ -f $(LSDK_FIRMWARE_BIN) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) QorIQ Firmware FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
+	@if ! [ -f $(LSDK_SRK_HASH) ]; then \
+		printf "***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) QorIQ Security Files *****\n\n"; \
+		wget -O $(LSDK_SRK_HASH) https://www.nxp.com/lgfiles/sdk/$(LSDK_VERSION_URL)/srk_hash.txt; \
+		if ! [ -f $(LSDK_SRK_HASH) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) $(LSDK_SRK_HASH) FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
+	@if ! [ -f $(LSDK_SRK_PRI) ]; then \
+		wget -O $(LSDK_SRK_PRI) https://www.nxp.com/lgfiles/sdk/$(LSDK_VERSION_URL)/srk.pri; \
+		if ! [ -f $(LSDK_SRK_PRI) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) $(LSDK_SRK_PRI) FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
+	@if ! [ -f $(LSDK_SRK_PUB) ]; then \
+		wget -O $(LSDK_SRK_PUB) https://www.nxp.com/lgfiles/sdk/$(LSDK_VERSION_URL)/srk.pub; \
+		if ! [ -f $(LSDK_SRK_PUB) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) $(LSDK_SRK_PUB) FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
 endef
 
 define bootloader-info-$(ET_BOARD_TYPE)
+	@printf "========================================================================\n"
+	@printf "LSDK_VERSION: $(LSDK_VERSION)\n"
+	@printf "LSDK_VERSION_URL: $(LSDK_VERSION_URL)\n"
+	@printf "LSDK_MACHINE: $(LSDK_MACHINE)\n"
+	@printf "LSDK_BOOTTYPE: $(LSDK_BOOTTYPE)\n"
+	@printf "LSDK_FIRMWARE_BIN: $(LSDK_FIRMWARE_BIN)\n"
+	@printf "LSDK_SRK_HASH: $(LSDK_SRK_HASH)\n"
+	@printf "LSDK_SRK_PRI: $(LSDK_SRK_PRI)\n"
+	@printf "LSDK_SRK_PUB: $(LSDK_SRK_PUB)\n"
 endef
--- boards/ls1043ardb/bootloader.mk
+++ boards/ls1043ardb/bootloader.mk
@@ -1,5 +1,11 @@
 include $(ET_DIR)/boards/$(ET_BOARD_TYPE)/bootloader.mk
 
+export LSDK_RCW_DIR := $(ET_SOFTWARE_DIR)/qoriq/rcw
+export LSDK_RCW_BIN ?= $(LSDK_RCW_DIR)/$(LSDK_MACHINE)/RR_FQPP_1455/rcw_1600_sdboot.bin
+export LSDK_ATF_DIR := $(ET_SOFTWARE_DIR)/qoriq/atf
+export LSDK_ATF_BL2_BIN ?= $(ET_SOFTWARE_DIR)/qoriq/atf/build/$(LSDK_MACHINE)/release/bl2_$(LSDK_BOOTTYPE).pbl
+export LSDK_ATF_FIP_BIN ?= $(LSDK_ATF_DIR)/build/$(LSDK_MACHINE)/release/fip.bin
+
 define bootloader-depends-$(ET_BOARD)
 	$(call bootloader-depends-$(ET_BOARD_TYPE))
 endef
@@ -10,8 +16,68 @@ endef
 
 define bootloader-finalize-$(ET_BOARD)
 	$(call bootloader-finalize-$(ET_BOARD_TYPE))
+	$(RM) $(ET_BOOTLOADER_DIR)/boot/bl2_$(LSDK_BOOTTYPE).pbl
+	$(RM) $(ET_BOOTLOADER_DIR)/boot/fip.bin
+	@mkdir -p $(ET_SOFTWARE_DIR)/qoriq
+	@if ! [ -d $(LSDK_RCW_DIR) ]; then \
+		(cd $(ET_SOFTWARE_DIR)/qoriq && \
+			git clone https://source.codeaurora.org/external/qoriq/qoriq-components/rcw $(LSDK_RCW_DIR)); \
+		if ! [ -d $(LSDK_RCW_DIR) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) QorIQ 'rcw' FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
+	@echo
+	(cd $(LSDK_RCW_DIR) && echo && \
+		git fetch --all && \
+		git checkout $(LSDK_VERSION) && echo && \
+		cd $(LSDK_MACHINE) && \
+			make clean all)
+	@if ! [ -f $(LSDK_RCW_BIN) ]; then \
+		printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Building $(LSDK_VERSION) $(LSDK_RCW_BIN) FAILED! *****\n\n"; \
+		exit 2; \
+	fi
+	@if ! [ -d $(LSDK_ATF_DIR) ]; then \
+		(cd $(ET_SOFTWARE_DIR)/qoriq && \
+			git clone https://source.codeaurora.org/external/qoriq/qoriq-components/atf $(LSDK_ATF_DIR)); \
+		if ! [ -d $(LSDK_ATF_DIR) ]; then \
+			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting $(LSDK_VERSION) QorIQ 'atf' FAILED! *****\n\n"; \
+			exit 2; \
+		fi; \
+	fi
+	@echo
+	(cd $(LSDK_ATF_DIR) && echo && \
+		git fetch --all && \
+		git checkout $(LSDK_VERSION) && echo && \
+		make PLAT=$(LSDK_MACHINE) clean && \
+		make ARCH=aarch64 CROSS_COMPILE=$(ET_CROSS_TUPLE)- \
+			PLAT=$(LSDK_MACHINE) bl2 \
+			BOOT_MODE=$(LSDK_BOOTTYPE) \
+			pbl RCW=$(LSDK_RCW_BIN))
+	@if ! [ -f $(LSDK_ATF_BL2_BIN) ]; then \
+		printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Building $(LSDK_VERSION) $(LSDK_ATF_BL2_BIN) FAILED! *****\n\n"; \
+		exit 2; \
+	fi
+	@echo
+	(cd $(LSDK_ATF_DIR) && echo && \
+		make ARCH=aarch64 CROSS_COMPILE=$(ET_CROSS_TUPLE)- \
+			PLAT=$(LSDK_MACHINE) fip \
+			BOOT_MODE=$(LSDK_BOOTTYPE) \
+			BL33=$(ET_BOOTLOADER_BUILD_IMAGE))
+	@if ! [ -f $(LSDK_ATF_FIP_BIN) ]; then \
+		printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Building $(LSDK_VERSION) $(LSDK_ATF_FIP_BIN) FAILED! *****\n\n"; \
+		exit 2; \
+	fi
+	@echo
+	@cp -av $(LSDK_ATF_BL2_BIN) $(ET_BOOTLOADER_DIR)/boot/
+	@cp -av $(LSDK_ATF_FIP_BIN) $(ET_BOOTLOADER_DIR)/boot/
 endef
 
 define bootloader-info-$(ET_BOARD)
 	$(call bootloader-info-$(ET_BOARD_TYPE))
+	@printf "LSDK_RCW_DIR: $(LSDK_RCW_DIR)\n"
+	@printf "LSDK_RCW_BIN: $(LSDK_RCW_BIN)\n"
+	@printf "LSDK_ATF_DIR: $(LSDK_ATF_DIR)\n"
+	@printf "LSDK_ATF_BL2_BIN: $(LSDK_ATF_BL2_BIN)\n"
+	@printf "LSDK_ATF_FIP_BIN: $(LSDK_ATF_FIP_BIN)\n"
 endef
