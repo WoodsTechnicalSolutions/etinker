07f9a68faa83ac0e846420d22483de6f2ce688f6
Derald D. Woods <woods.technical@gmail.com>
2021-12-17 17:13:56 -0600

    boards: meson: use 'gxlimg' for u-boot image signing
    This fixes issue created by passing 'u-boot.bin' instead of
    'u-boot-dtb.bin' to the gxlimg tool.

--- boards/meson/bootloader.mk
+++ boards/meson/bootloader.mk
@@ -1,13 +1,7 @@
 export ET_BOOTLOADER_ARCH := arm
 
-ifeq ($(shell echo $(ET_BOARD_TYPE) | grep -Po libre),libre)
-# Libre Computer 'libretech-u-boot' tree
-export ET_BOOTLOADER_VERSION := $(shell cd $(ET_BOOTLOADER_SOFTWARE_DIR) 2>/dev/null && make -s ubootversion | tr -d \\n)
-export ET_BOOTLOADER_LOCALVERSION := -libretech
-endif
-
-export FIP_DIR := $(ET_SOFTWARE_DIR)/fip
-export FIP_BUILD_DIR := $(ET_BOOTLOADER_BUILD_DIR)/fip
+export FIP_DIR := $(ET_SOFTWARE_DIR)/gxlimg
+export FIP_BUILD_DIR := $(FIP_DIR)/build/fip/gxl
 
 define bootloader-depends-$(ET_BOARD_TYPE)
 endef
@@ -15,66 +9,16 @@ endef
 define bootloader-prepare-$(ET_BOARD_TYPE)
 endef
 
-# https://github.com/u-boot/u-boot/blob/master/doc/board/amlogic/libretech-cc.rst
+# https://github.com/repk/gxlimg [BSD-2-Clause]
 define bootloader-finalize-$(ET_BOARD_TYPE)
-	@if ! [ -d $(FIP_DIR) ]; then \
-		(cd $(ET_SOFTWARE_DIR) && \
-			wget https://github.com/BayLibre/u-boot/releases/download/v2017.11-libretech-cc/libretech-cc_fip_20180418.tar.gz && \
-			tar -zxvf libretech-cc_fip_20180418.tar.gz); \
-		if ! [ -f $(FIP_DIR)/gxl/aml_encrypt_gxl ]; then \
-			printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] Getting Amlogic binary blobs FAILED! *****\n\n"; \
-			exit 2; \
-		fi; \
-	fi
-	@mkdir -p $(FIP_BUILD_DIR)
-	@cp -av $(FIP_DIR)/gxl/bl2.bin $(FIP_BUILD_DIR)/
-	@cp -av $(FIP_DIR)/gxl/acs.bin $(FIP_BUILD_DIR)/
-	@cp -av $(FIP_DIR)/gxl/bl21.bin $(FIP_BUILD_DIR)/
-	@cp -av $(FIP_DIR)/gxl/bl30.bin $(FIP_BUILD_DIR)/
-	@cp -av $(FIP_DIR)/gxl/bl301.bin $(FIP_BUILD_DIR)/
-	@cp -av $(FIP_DIR)/gxl/bl31.img $(FIP_BUILD_DIR)/
-	@cp -av $(ET_BOOTLOADER_BUILD_IMAGE) $(FIP_BUILD_DIR)/bl33.bin
-	$(FIP_DIR)/blx_fix.sh \
-		$(FIP_BUILD_DIR)/bl30.bin \
-		$(FIP_BUILD_DIR)/zero_tmp \
-		$(FIP_BUILD_DIR)/bl30_zero.bin \
-		$(FIP_BUILD_DIR)/bl301.bin \
-		$(FIP_BUILD_DIR)/bl301_zero.bin \
-		$(FIP_BUILD_DIR)/bl30_new.bin \
-		bl30
-	python2 $(FIP_DIR)/acs_tool.pyc $(FIP_BUILD_DIR)/bl2.bin $(FIP_BUILD_DIR)/bl2_acs.bin $(FIP_BUILD_DIR)/acs.bin 0
-	$(FIP_DIR)/blx_fix.sh \
-		$(FIP_BUILD_DIR)/bl2_acs.bin \
-		$(FIP_BUILD_DIR)/zero_tmp \
-		$(FIP_BUILD_DIR)/bl2_zero.bin \
-		$(FIP_BUILD_DIR)/bl21.bin \
-		$(FIP_BUILD_DIR)/bl21_zero.bin \
-		$(FIP_BUILD_DIR)/bl2_new.bin \
-		bl2
-	$(FIP_DIR)/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_BUILD_DIR)/bl30_new.bin
-	$(FIP_DIR)/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_BUILD_DIR)/bl31.img
-	$(FIP_DIR)/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_BUILD_DIR)/bl33.bin
-	$(FIP_DIR)/gxl/aml_encrypt_gxl --bl2sig --input $(FIP_BUILD_DIR)/bl2_new.bin --output $(FIP_BUILD_DIR)/bl2.n.bin.sig
-	$(FIP_DIR)/gxl/aml_encrypt_gxl --bootmk \
-		--output $(FIP_BUILD_DIR)/u-boot.bin \
-		--bl2    $(FIP_BUILD_DIR)/bl2.n.bin.sig \
-		--bl30   $(FIP_BUILD_DIR)/bl30_new.bin.enc \
-		--bl31   $(FIP_BUILD_DIR)/bl31.img.enc \
-		--bl33   $(FIP_BUILD_DIR)/bl33.bin.enc
-	@cp -av $(FIP_BUILD_DIR)/u-boot.bin* $(ET_BOOTLOADER_DIR)/boot/
-endef
-
-define bootloader-finalize-$(ET_BOARD_TYPE)-gxlimg
 	$(call software-check,gxlimg,gxlimg)
-	$(call software-check,arm-trusted-firmware,arm-trusted-firmware)
-	@$(RM) $(ET_BOOTLOADER_DIR)/boot/u-boot*
 	@(cd $(ET_SOFTWARE_DIR)/gxlimg && \
 		$(MAKE) --no-print-directory \
 			clean \
 			image \
-			UBOOT=$(ET_BOOTLOADER_BUILD_IMAGE) && \
-		cp -av build/fip/gxl/u-boot.bin.enc $(ET_BOOTLOADER_DIR)/boot/u-boot.bin && \
-		cp -av build/gxl-boot.bin $(ET_BOOTLOADER_DIR)/boot/)
+			UBOOT=$(ET_BOOTLOADER_BUILD_DIR)/u-boot-dtb.bin && \
+		cp -av $(FIP_BUILD_DIR)/u-boot.bin.enc $(ET_BOOTLOADER_DIR)/boot/u-boot.bin && \
+		cp -av $(FIP_DIR)/build/gxl-boot.bin $(ET_BOOTLOADER_DIR)/boot/)
 endef
 
 define bootloader-info-$(ET_BOARD_TYPE)
