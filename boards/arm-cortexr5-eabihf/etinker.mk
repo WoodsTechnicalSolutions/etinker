2292a4eabaf32c75b38545d1a36882ac39485b71
Derald D. Woods <woods.technical@gmail.com>
2023-12-25 10:54:53 -0500

    boards: jacinto: add toolchains for SK-TDA4VM
    https://www.ti.com/tool/SK-TDA4VM
    
    A 2023 Christmas gift to myself. Still tinkering @ 50 years old!

---
+++ boards/arm-cortexr5-eabihf/etinker.mk
@@ -0,0 +1,41 @@
+#
+# arm-cortexr5-eabihf toolchain configuration file for 'etinker'
+#
+# Copyright (C) 2023, Derald D. Woods <woods.technical@gmail.com>
+#
+# [references]
+# ------------------------------------------------------------------------------
+# https://www.arm.com/products/silicon-ip-cpu/cortex-r/cortex-r5
+# https://developer.arm.com/ip-products/processors/cortex-r/cortex-r5
+# https://en.wikipedia.org/wiki/ARM_Cortex-R
+# ------------------------------------------------------------------------------
+#
+# This file is made available under the Creative Commons CC0 1.0
+# Universal Public Domain Dedication.
+#
+# The person who associated a work with this deed has dedicated
+# the work to the public domain by waiving all of his or her rights
+# to the work worldwide under copyright law, including all related
+# and neighboring rights, to the extent allowed by law. You can copy,
+# modify, distribute and perform the work, even for commercial purposes,
+# all without asking permission.
+#
+# The board configs are intended to be adapted for use with your project,
+# which may be proprietary.  So unlike the project itself, they are
+# licensed Public Domain.
+#
+
+ET_BOARD_ARCH := arm
+ET_BOARD_VENDOR := cortexr5
+ET_BOARD_ABI := eabihf
+ET_BOARD_CROSS_TUPLE := $(ET_BOARD_ARCH)-$(ET_BOARD_VENDOR)-$(ET_BOARD_ABI)
+
+ET_BOARD_TOOLCHAIN_TREE := crosstool-ng
+
+ifeq ($(ET_BOARD),$(ET_BOARD_CROSS_TUPLE))
+export ET_BOARD_TYPE := $(ET_BOARD_CROSS_TUPLE)
+endif
+
+export ET_MCU_LIBC ?= newlib-nano
+
+export CT_KERNEL = bare-metal
