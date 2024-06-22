d0231b20efa7a9fbf6e66e0a97cce4fc85305f9f
Derald D. Woods <woods.technical@gmail.com>
2024-06-22 19:08:26 -0400

    boards/k3-r5: move tiboot3.bin check into shell

--- boards/k3/bootloader.mk
+++ boards/k3/bootloader.mk
@@ -1,10 +1,11 @@
 include $(ET_DIR)/boards/k3/common.mk
 
 define bootloader-depends-$(ET_BOARD_TYPE)
-ifneq (exists,$(shell test -f $(ET_DIR)/bootloader/k3-$(TI_K3_SOC)-r5/$(TI_R5_CROSS_TUPLE)/boot/tiboot3.bin && echo exists))
-$(error ET_BOARD=k3-$(TI_K3_SOC)-r5 MUST BE BUILT FIRST! ***)
-endif
 	@$(call bootloader-depends-common)
+	@if [ ! -f "$(ET_DIR)/bootloader/k3-$(TI_K3_SOC)-r5/$(TI_R5_CROSS_TUPLE)/boot/tiboot3.bin" ]; then \
+		printf "***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] ET_BOARD=k3-$(TI_K3_SOC)-r5 MUST BE BUILT FIRST! *****\n\n"; \
+		false; \
+	fi
 endef
 
 define bootloader-prepare-$(ET_BOARD_TYPE)
