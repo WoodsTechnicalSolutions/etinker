f17a9ef881f76bbfbbd5abbcad67774eb1649677
Derald D. Woods <woods.technical@gmail.com>
2024-08-25 19:48:09 -0400

    packages/cryptodev-linux.mk: use new combined 'install' target

--- packages/cryptodev-linux.mk
+++ packages/cryptodev-linux.mk
@@ -43,7 +43,7 @@ endef
 define cryptodev-linux-targets
 	@printf "\n***** [$(ET_BOARD)][$(ET_BOARD_TYPE)] cryptodev-linux *****\n\n"
 	$(call cryptodev-linux-build,build)
-	$(call cryptodev-linux-build,modules_install)
+	$(call cryptodev-linux-build,install)
 endef
 
 define cryptodev-linux-build
